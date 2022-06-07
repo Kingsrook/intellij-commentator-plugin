29e336fd11c17e692ef150338a43c8752f5e9547
Darin Kelkhoff <darin.kelkhoff@kingsrook.com>
2022-06-07 09:13:47 -0500

    Updating language level to 15 (to do text-blocks in tests); adding junit 5 to gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,11 +3,20 @@ plugins {
     id("org.jetbrains.intellij") version "1.5.2"
 }
 
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+}
+
 group = "com.kingsrook"
 version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+}
+java {
+    sourceCompatibility = JavaVersion.VERSION_15
+    targetCompatibility = JavaVersion.VERSION_15
 }
 
 // Configure Gradle IntelliJ Plugin - read more: https://github.com/JetBrains/gradle-intellij-plugin
@@ -21,8 +30,8 @@ intellij {
 tasks {
     // Set the JVM compatibility versions
     withType<JavaCompile> {
-        sourceCompatibility = "11"
-        targetCompatibility = "11"
+        sourceCompatibility = "15"
+        targetCompatibility = "15"
     }
 
     patchPluginXml {
@@ -38,5 +47,12 @@ tasks {
 
     publishPlugin {
         token.set(System.getenv("PUBLISH_TOKEN"))
+    }
+
+    test {
+        useJUnitPlatform()
+        testLogging {
+            showStandardStreams = true
+        }
     }
 }
